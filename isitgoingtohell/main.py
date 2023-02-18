a78bbcf03e392eb7154315142001a35e4de9c338
Nimotomato <108477753+nimotomato@users.noreply.github.com>
2023-02-18 08:55:35 +0100

    start import scrapers

--- isitgoingtohell/main.py
+++ isitgoingtohell/main.py
@@ -1,1 +1,14 @@
-print("Hellul world!")
+from logging import getLogger
+
+from isitgoingtohell.scraping.scrape_news import scrape_news
+
+log = getLogger("Main")
+
+
+def main():
+    print("Hellul world!")
+    log.info("scraping news...")
+    scrape_news()
+
+if __name__ == "__main__":
+    main()
