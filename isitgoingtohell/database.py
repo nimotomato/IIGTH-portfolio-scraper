40d0e24be008ea5e9f155becba98dfb6818f4f05
Nimotomato <108477753+nimotomato@users.noreply.github.com>
2023-02-19 09:09:22 +0100

    database and sentiment analyser up and running

--- isitgoingtohell/database.py
+++ isitgoingtohell/database.py
@@ -1,4 +1,5 @@
 import os
+from typing import List
 
 import pandas as pd
 from sqlalchemy import create_engine, text
@@ -32,33 +33,7 @@ def upload_news(news_df: pd.DataFrame) -> int:
     return n_uploads
 
 
-def get_unanalysed_news() -> pd:
-    """get unanalysed data from the database"""
-    url = (
-        f"postgresql+psycopg2://{os.environ['DB_USERNAME']}:{os.environ['DB_PASSWORD']}"
-        f"@{os.environ['DB_HOST']}:{os.environ['DB_PORT']}/{os.environ['DB_NAME']}"
-    )
-    engine = create_engine(
-        url,
-        echo=False,
-    )
-
-
-    query = (
-        "SELECT news.* FROM news LEFT JOIN sentiments ON news.id = sentiments.news_id WHERE sentiments.news_id IS NULL;"
-    )
-
-
-    with engine.connect() as connection:
-        news_df = pd.read_sql_query(text(query), connection)
-    
-
-    news_df.set_index('id')
-    
-    return news_df
-
-
-def upload_analysed_news(analysed_news_df: pd.DataFrame) -> int:
+def upload_sentiments(analysed_news_df: pd.DataFrame) -> int:
     """uploads analysed data to sentiments table in database"""
     url = (
         f"postgresql+psycopg2://{os.environ['DB_USERNAME']}:{os.environ['DB_PASSWORD']}"
@@ -71,42 +46,42 @@ def upload_analysed_news(analysed_news_df: pd.DataFrame) -> int:
 
 
     n_uploads = 0
-    with engine.connect() as conn:
+    with engine.connect() as connection:
         for _, row in analysed_news_df.iterrows():
             query = (
-                f"INSERT INTO sentiments (news_id, label, score)"
-                f"VALUES {tuple(row.to_dict().values())} ON CONFLICT (headline) DO NOTHING;"
+                f"INSERT INTO sentiments (label, score, news_id)"
+                f"VALUES {tuple(row.to_dict().values())} ON CONFLICT (news_id) DO NOTHING;"
             )
-            r = conn.execute(text(query))
+            response = connection.execute(text(query))
 
-            # if we add an item rowcount is 1, if its a dupliacte its 0
-            n_uploads += r.rowcount
+            # if we add an item rowcount is 1, if its a duplicate its 0
+            n_uploads += response.rowcount
+        connection.commit()
 
     return n_uploads
 
 
-# def upload_data(analysed_news_df: pd.DataFrame) -> int:
-#     """uploads all data to postgres db
-#     see schema in schema/news"""
-#     url = (
-#         f"postgresql+psycopg2://{os.environ['DB_USERNAME']}:{os.environ['DB_PASSWORD']}"
-#         f"@{os.environ['DB_HOST']}:{os.environ['DB_PORT']}/{os.environ['DB_NAME']}"
-#     )
-#     engine = create_engine(
-#         url,
-#         echo=False,
-#     )
+def get_unanalysed_news() -> List[tuple]:
+    """get unanalysed data from the database"""
+    url = (
+        f"postgresql+psycopg2://{os.environ['DB_USERNAME']}:{os.environ['DB_PASSWORD']}"
+        f"@{os.environ['DB_HOST']}:{os.environ['DB_PORT']}/{os.environ['DB_NAME']}"
+    )
+    engine = create_engine(
+        url,
+        echo=False,
+    )
 
-#     n_uploads = 0
-#     with engine.connect() as conn:
-#         for _, row in analysed_news_df.iterrows():
-#             query = (
-#                 f"INSERT INTO news (headline, date, region, source, label, score)"
-#                 f"VALUES {tuple(row.to_dict().values())} ON CONFLICT (headline) DO NOTHING;"
-#             )
-#             r = conn.execute(text(query))
 
-#             # if we add an item rowcount is 1, if its a dupliacte its 0
-#             n_uploads += r.rowcount
+    # Joins news and sentiments on news, but only for news with no sentiments (unanalysed news)
+    query = (
+        "SELECT news.ID, news.headline FROM news LEFT JOIN sentiments ON news.id = sentiments.news_id WHERE sentiments.news_id IS NULL;"
+    )
 
-#     return n_uploads
+
+    with engine.connect() as connection:
+        #news_df = pd.read_sql_query(text(query), connection)
+        news_df = connection.execute(text(query))
+
+
+    return news_df.fetchall()
